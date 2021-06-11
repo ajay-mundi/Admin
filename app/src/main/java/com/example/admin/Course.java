a2adb8901588902d47c0edf10c909e44a12c134b
ajaym <ajay.mundi@gmail.com>
2021-06-10 20:48:45 -0400

    Added simple list view with input arraylist of type string

---
+++ app/src/main/java/com/example/admin/Course.java
@@ -0,0 +1,20 @@
+package com.example.admin;
+
+public class Course {
+    private String coarseName;
+
+    public Course(String coarseName){
+        this.coarseName = coarseName;
+    }
+
+    public String getCoarseName(){
+        return coarseName;
+    }
+
+    public void setCoarseName(String coarseName){
+        this.coarseName = coarseName;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/admin/recyclerAdapter.java
@@ -0,0 +1,54 @@
+package com.example.admin;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class recyclerAdapter extends RecyclerView.Adapter<recyclerAdapter.MyViewHolder> {
+    private ArrayList<Course> courseList;
+
+    public recyclerAdapter(ArrayList<Course> coursesList){
+        this.courseList =coursesList;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        private TextView nameTxt;
+
+        public MyViewHolder(final View view){
+            super(view);
+            nameTxt = view.findViewById(R.id.textView);
+        }
+
+    }
+
+
+
+
+    @NonNull
+    @NotNull
+    @Override
+    public recyclerAdapter.MyViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_items, parent,false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull recyclerAdapter.MyViewHolder holder, int position) {
+        String name = courseList.get(position).getCoarseName();
+        holder.nameTxt.setText(name);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return courseList.size();
+    }
+}
