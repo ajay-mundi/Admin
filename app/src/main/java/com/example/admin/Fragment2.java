1600e55af8ff188fd3d785376be275133d36b0a1
ajay-mundi <65574040+ajay-mundi@users.noreply.github.com>
2021-06-10 20:50:13 -0400

    Added simple list view with input arraylist of type string (#1)

--- app/src/main/java/com/example/admin/Fragment2.java
+++ app/src/main/java/com/example/admin/Fragment2.java
@@ -1,21 +1,46 @@
 package com.example.admin;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Fragment2 extends Fragment {
+
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
-    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment2_layout,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //recyclerView = recyclerView.findViewById(R.id.recycle);
+        //listView = listView.findViewById(R.id.listview);
+        View v = inflater.inflate(R.layout.fragment2_layout,container,false);
+        //View v = inflater.inflate(R.layout.list_items,container,false);
+
+        //setAdapter();
+        return v;
     }
+
+   /* private void setAdapter() {
+        recyclerAdapter adapter = new recyclerAdapter(courseList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context.getApplicationContext());
+        //recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }*/
 }
--- app/src/main/java/com/example/admin/Fragment3.java
+++ app/src/main/java/com/example/admin/Fragment3.java
@@ -1,9 +1,12 @@
 package com.example.admin;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +14,31 @@ import androidx.fragment.app.Fragment;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+
+
 public class Fragment3 extends Fragment {
+    private ArrayList<Course> courseList;
+    //ArrayAdapter<Course> adapter;
+    //private RecyclerView recyclerView;
+    ListView listView;
+    Activity context;
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment3_layout,container,false);
+        View v = inflater.inflate(R.layout.fragment3_layout,container,false);
+        listView = v.findViewById(R.id.listview);
+
+        String[] display = {"Ajay", "Manvir", "Alex"};
+        courseList = new ArrayList<Course>();
+        courseList.add(new Course("CVG2031"));
+        courseList.add(new Course("GNG2432"));
+        courseList.add(new Course("SEG4331"));
+        //ArrayAdapter<Course> adapter = new ArrayAdapter<Course>(context.getApplicationContext(), android.R.layout.simple_list_item_1, courseList);
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,display);
+        listView.setAdapter(listViewAdapter);
+        //listView.setAdapter(adapter);
+        return v;
     }
 }
