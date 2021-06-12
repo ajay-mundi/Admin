75da35ec5bbf76b0281d10ba9bd49ab2771113b8
ajay-mundi <65574040+ajay-mundi@users.noreply.github.com>
2021-06-12 15:50:01 -0400

    Added add course functionality

--- app/src/main/java/com/example/admin/Fragment3.java
+++ app/src/main/java/com/example/admin/Fragment3.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,16 +22,17 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 
 
-public class Fragment3 extends Fragment {
+public class Fragment3 extends Fragment{
     private ArrayList<Course> courseList;
     ListView listView;
     Activity context;
+    Button add;
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment3_layout,container,false);
-        //View vr = inflater.inflate(R.layout.class_activity,container,false);
+        add = v.findViewById(R.id.Add);
         listView = v.findViewById(R.id.listview);
 
 
@@ -38,15 +40,16 @@ public class Fragment3 extends Fragment {
         courseList.add(new Course("Hydraulics","CVG2031"));
         courseList.add(new Course("Engineering Design","GNG2432"));
         courseList.add(new Course("Software Design","SEG4331"));
-        courseList.add(new Course("Hydraulics","CVG2031"));
-        courseList.add(new Course("Engineering Design","GNG2432"));
-        courseList.add(new Course("Software Design","SEG4331"));
-        courseList.add(new Course("Hydraulics","CVG2031"));
-        courseList.add(new Course("Engineering Design","GNG2432"));
-        courseList.add(new Course("Software Design","SEG4331"));
+        courseList.add(new Course("Hydrology","CVG3344"));
+        courseList.add(new Course("Psychology","PSY2432"));
+        courseList.add(new Course("Roman Civilization","SEG4331"));
+        courseList.add(new Course("Structural Dynamics","CVG4444"));
+        courseList.add(new Course("Digital Systems","ITI1100"));
+        courseList.add(new Course("Calculus 2","MAT2477"));
 
         courseListAdapter adapter = new courseListAdapter(getActivity(), R.layout.adapter_view_layout, courseList);//used this.context instead of this
         listView.setAdapter(adapter);
+
 
         //
         listView.setClickable(true);
@@ -56,12 +59,6 @@ public class Fragment3 extends Fragment {
               Course selected = courseList.get(position);
               String courseName = selected.getCourseName();
               String courseCode = selected.getCourseCode();
-              /*TextView coName = vr.findViewById(R.id.textView8);
-              TextView coCode = vr.findViewById(R.id.textView7);
-
-              coName.setText(courseName);
-              coName.append("h");
-              coCode.setText(courseCode);*/
               Intent intent = new Intent(getActivity(), CourseActivity.class);
               intent.putExtra("CourseName", courseName);
               intent.putExtra("CourseCode", courseCode);
@@ -71,7 +68,19 @@ public class Fragment3 extends Fragment {
           }
         });
 
-
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View r) {
+                TextView nameIn = v.findViewById(R.id.CourseName);
+                String name = nameIn.getText().toString();
+                TextView codeIn = v.findViewById(R.id.CourseCode);
+                String code = codeIn.getText().toString();
+                courseList.add(new Course(name, code));
+                adapter.notifyDataSetChanged();
+            }
+        });
         return v;
     }
+
+
 }
